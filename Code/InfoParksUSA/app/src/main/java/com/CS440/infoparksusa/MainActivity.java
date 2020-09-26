5a84733b42acf75bff1fd7a56f7f051afac78bf8
Claudio Jimenez <clausji@mac.com>
2020-09-26 13:36:56 -0500

    Weekly minutes update, changed some code on InfoParks

--- Code/InfoParksUSA/app/src/main/java/com/CS440/infoparksusa/MainActivity.java
+++ Code/InfoParksUSA/app/src/main/java/com/CS440/infoparksusa/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -15,6 +16,22 @@ public class MainActivity extends AppCompatActivity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button button = (Button) findViewById(R.id.create_account_button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    Intent intent = new Intent(MainActivity.this,UserInfoActivity.class);
+                    startActivity(intent);
+                }
+                catch (Exception e){
+                    Log.e("error:", e.toString());
+                }
+
+            }
+        });
 
 
     }
