93f5a5b106274df013c452ea8a7e350a41e4df0e
Claudio Jimenez <clausji@mac.com>
2020-10-04 21:04:36 -0500

    Added Activity
    Added choose your park activity. The parks are linked with the google API

---
+++ Code/InfoParksUSA/app/src/main/java/com/CS440/infoparksusa/ChooseYourParkListView.java
@@ -0,0 +1,59 @@
+package com.CS440.infoparksusa;
+
+import android.app.ListActivity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
+
+public class ChooseYourParkListView extends ListActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+        setListAdapter(new ArrayAdapter<String>(this,
+                R.layout.activity_choose_your_park_list_view,
+                getResources().getStringArray(R.array.national_parks)));
+    }
+
+
+    public void onListItemClick(ListView parent, View view,
+                                int position, long id) {
+
+        try {
+
+            // Process text for network transmission
+            final TextView textView = (TextView) view;
+            String address = (String) textView.getText();
+            address = address.replace(' ', '+');
+
+            // Create Intent object for starting Google Maps application
+            Intent geoIntent = new Intent(
+                    Intent.ACTION_VIEW,
+                    Uri.parse("geo:0,0?q=" + address));
+
+            if(geoIntent.resolveActivity(getPackageManager()) != null) {
+                // Use the Intent to start Google Maps application using Activity.startActivity()
+                startActivity(geoIntent);
+
+                Log.i("joder" + " From Ugo", "Map activity just started");
+            }
+        } catch (Exception e) {
+            // Log any error messages to LogCat using Log.e()
+            Log.e("catch", e.toString());
+        }
+
+    }
+
+
+}
--- Code/InfoParksUSA/app/src/main/java/com/CS440/infoparksusa/UserInfoActivity.java
+++ Code/InfoParksUSA/app/src/main/java/com/CS440/infoparksusa/UserInfoActivity.java
@@ -2,7 +2,9 @@ package com.CS440.infoparksusa;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class UserInfoActivity extends AppCompatActivity {
@@ -11,5 +13,20 @@ public class UserInfoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_info);
+
+        final Button button = (Button) findViewById(R.id.done_button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(UserInfoActivity.this, ChooseYourParkListView.class);
+                startActivity(intent);
+            }
+        });
+
+
+
     }
+
+
 }
