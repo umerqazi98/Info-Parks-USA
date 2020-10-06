42a188f5921f5bca805f9dba2b86f8192f027ed5
Claudio Jimenez <clausji@mac.com>
2020-10-06 11:11:16 -0500

    updated some files with try/catch statement

--- Code/InfoParksUSA/app/src/main/java/com/CS440/infoparksusa/UserInfoActivity.java
+++ Code/InfoParksUSA/app/src/main/java/com/CS440/infoparksusa/UserInfoActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -19,8 +20,15 @@ public class UserInfoActivity extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(UserInfoActivity.this, ChooseYourParkListView.class);
-                startActivity(intent);
+                try {
+                    Intent intent = new Intent(UserInfoActivity.this, ChooseYourParkListView.class);
+                    startActivity(intent);
+
+                }
+                catch (Exception e){
+                    Log.e("error:UserinfoActivity", e.toString());
+                }
+
             }
         });
 
