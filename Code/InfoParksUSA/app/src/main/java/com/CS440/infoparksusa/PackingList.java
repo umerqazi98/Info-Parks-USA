997b71ca7f69792ff2674b829101a3b803d5485b
Syed Raza <sraza21@uic.edu>
2020-10-28 09:47:36 -0500

    listview, button, textfield

--- Code/InfoParksUSA/app/src/main/java/com/CS440/infoparksusa/PackingList.java
+++ Code/InfoParksUSA/app/src/main/java/com/CS440/infoparksusa/PackingList.java
@@ -3,14 +3,26 @@ package com.CS440.infoparksusa;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class PackingList extends AppCompatActivity {
-
+    private ArrayList<String> items;
+    private ArrayAdapter<String> itemsAdapter;
+    private ListView lvItems;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_packing_list);
-
+        lvItems = (ListView) findViewById(R.id.lvItems);
+        items = new ArrayList<String>();
+        itemsAdapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, items);
+        lvItems.setAdapter(itemsAdapter);
+        items.add("Tent");
+        items.add("Bear Spray");
         // will need to setup listview for items
     }
 }
